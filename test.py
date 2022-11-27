9b060fa810ad24d4eb0fe34e795963f704e84b08
Miroslav Kirnak <106260045+Mirda81@users.noreply.github.com>
2022-11-27 09:47:47 +0100

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import prace_se_soubory as soubory
-
-novy_list = [{'jméno': 'Mirda', 'skore': 1050}, {'jméno': 'kokot', 'skore': 5000}]
-# aktualizace = soubory.aktualizuj_nejlepsi_hry(novy_list)
-# soubory.uloz_vysledky('vysledky_jednotlive',aktualizace)
-# print(soubory.nacti_vysledky('vysledky_jednotlive'))
-# print(soubory.aktualizuj_nejlepsi_hrace(novy_list))
-print(soubory.vypis_top_ten('historie_hracu'))
